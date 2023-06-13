ce31621d8f36a9736d75cbcfdec3387e5c8c7113
kaydashhh <125455856+kaydashhh@users.noreply.github.com>
2023-06-13 12:15:28 +0100

    Update main.tf

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region = "us-east-1"
-  profile= "austinobioma-realcloud"
+  profile= "default"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "austinobioma-docker-tfstate"
+  bucket = "docker-tfstate"
      
   lifecycle {
     prevent_destroy = true
